4f1fdee1b6eb1ce5751d87c51e14f34f60cf87c0
Ahmed Mustafa <Ahmed_Mustafa_1251@fci.helwan.edu.eg>
2025-09-18 16:31:26 +0300

    new updates to the code

--- src/embedding_system.py
+++ src/embedding_system.py
@@ -23,7 +23,7 @@ except ImportError:
 
 
 
-SILICONFLOW_API_KEY = os.environ['SILICONFLOW_API_KEY']
+SILICONFLOW_API_KEY = os.getenv('SILICONFLOW_API_KEY', 'sk-mamyyymhoyklygepxyaazxpxiaphjjbbynxgdrzebbmusmwl')
 @dataclass
 class EmbeddingResult:
     """Result of embedding generation."""
